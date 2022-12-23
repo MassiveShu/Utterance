6eb0180c187891694cb572851dc02aaecbe7ff4f
MassiveShu <shulyakmaksim@gmail.com>
2022-12-23 13:55:17 +0200

    Implement OnboardingStarsAnimation

---
+++ Utterance/OnboardingStarsAnimation.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingStarsAnimation.swift
+//  Utterance
+//
+//  Created by Max Shu on 23.12.2022.
+//
+
+import SwiftUI
+
+struct OnboardingStarsAnimation: View {
+    var body: some View {
+        Text("..")
+    }
+}
+
+struct OnboardingStarsAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingStarsAnimation()
+    }
+}
