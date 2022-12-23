0751ea0fa28b6dd59b0ec973915ca52f2a7197d1
MassiveShu <shulyakmaksim@gmail.com>
2022-12-23 14:51:37 +0200

    Demo of animation

--- Utterance/ContentView.swift
+++ Utterance/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Srart")
-        }
-        .padding()
+//        VStack {
+            OnboardingStarsAnimation()
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Srart")
+//        }
+//        .padding()
     }
 }
 
--- Utterance/OnboardingStarsAnimation.swift
+++ Utterance/OnboardingStarsAnimation.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct OnboardingStarsAnimation: View {
+    @State private var runAnimation = false
     var body: some View {
-        Text("..")
+        ZStack {
+            // create a black background with no edges ignoredSafeArea
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                ForEach(0..<10) { _ in
+                    HStack {
+                        ForEach(0..<5) { _ in
+                            Circle().fill(Color.white)
+                            // size of stars
+                                .frame(width: 3, height: 2)
+                                .blur(radius: runAnimation ? 1 : 0)
+                            // animate stars
+                                .animation(Animation.easeInOut(duration: 13).repeatForever())
+                                .padding(EdgeInsets(
+                                    top: self.random(),
+                                    leading: 0,
+                                    bottom: 0,
+                                    trailing: self.random())
+                                )
+                                .onAppear() {
+                                    self.runAnimation = true
+                                }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    func random() -> CGFloat {
+        return CGFloat(Int.random(in: 30..<200))
     }
 }
 
