92c5b52136d5dd4d10ef071de0e9def0049f96d8
MassiveShu <shulyakmaksim@gmail.com>
2022-12-23 17:46:57 +0200

    Implement Play and Pause Buttons

---
+++ Utterance/StopButtonStyle.swift
@@ -0,0 +1,19 @@
+//
+//  StopButtonStyle.swift
+//  Utterance
+//
+//  Created by Max Shu on 23.12.2022.
+//
+
+import SwiftUI
+
+struct StopButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.largeTitle.bold())
+            .background(configuration.isPressed ? Color.yellow : .blue)
+            .foregroundColor(Color.white)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+            .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+    }
+}
