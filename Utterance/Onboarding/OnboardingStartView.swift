f4361d22b0ae1ff69feae1e371cc9521b5c590df
MassiveShu <shulyakmaksim@gmail.com>
2022-12-24 14:48:02 +0200

    implement OnboardingStartView

---
+++ Utterance/Onboarding/OnboardingStartView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingStartView.swift
+//  Utterance
+//
+//  Created by Max Shu on 24.12.2022.
+//
+
+import SwiftUI
+
+struct OnboardingStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingStartView()
+    }
+}
