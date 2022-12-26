590fd35f5ee330e3edd59e6bbb1cab8751d4d4d9
MassiveShu <shulyakmaksim@gmail.com>
2022-12-26 18:46:56 +0200

    Implement a OnboardingSecondView

---
+++ Utterance/Onboarding/OnboardingSecondView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingSecondView.swift
+//  Utterance
+//
+//  Created by Max Shu on 26.12.2022.
+//
+
+import SwiftUI
+
+struct OnboardingSecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingSecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingSecondView()
+    }
+}
