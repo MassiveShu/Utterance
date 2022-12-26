18c84d1adf292197427d4a48814698d527ac1cd6
MassiveShu <shulyakmaksim@gmail.com>
2022-12-26 18:54:30 +0200

    first commit

--- Utterance/Onboarding/OnboardingSecondView.swift
+++ Utterance/Onboarding/OnboardingSecondView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct OnboardingSecondView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            OnboardingStarsAnimation()
+        }
     }
 }
 
