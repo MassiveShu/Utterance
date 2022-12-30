1cb54381927e151ee3718fa4483ac0d2f38f408f
MassiveShu <shulyakmaksim@gmail.com>
2022-12-30 13:42:41 +0200

    Delete comments in VM

--- Utterance/View/UtteranceConfigurationView.swift
+++ Utterance/View/UtteranceConfigurationView.swift
@@ -49,7 +49,7 @@ struct UtteranceConfigurationView: View {
                     VolumeSlider(activeVolume: $viewModel.activeVolume)
 
                     RateStepper(rateValue: $viewModel.activeRate)
-                    
+
                     PitchStepper(pitchValue: $viewModel.activePitch)
                 }
                 .padding()
